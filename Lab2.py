1785c1af65a5bdc50208b026320989c62ee201d9
Van Le <minhmihnle@gmail.com>
2021-06-23 16:03:34 -0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,22 @@
+name = "LeBron"
+#string (str)
+age = 35
+#integer (int)
+salary = 37.44
+# float
+plays_basketball = True
+#boolean 
+jersey_number = "23"
+
+car_name = "Tesla"
+#String (str)
+x = 50 
+#integer (int)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
